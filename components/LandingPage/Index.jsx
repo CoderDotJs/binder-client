9726420b8fd4d1025bf607daa8b9056cc44222c8
CoderDotJs <md.aksh6724@gmail.com>
2022-06-13 17:45:46 +0600

    landing page

--- components/LandingPage/Index.jsx
+++ components/LandingPage/Index.jsx
@@ -124,29 +124,253 @@ const Index = () => {
       </div>
 
       {/* reviews slider */}
-      <div ref={sliderRef} className="container mx-auto my-10 keen-slider h-52">
+      <div ref={sliderRef} className="container mx-auto my-10 keen-slider">
         <div className="keen-slider__slide number-slide">
-          <div className=" p-5 bg-white">
-            <div className=" flex justify-between">
-              <div className="w-8/12">
-                <h3 className="text-sm text-black ">Md Akash</h3>
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
                 <div className="divider my-0"></div>
               </div>
-              <div className="pl-2 w-3/12">
+              <div className=" w-2/12">
                 <p className=" text-slate-400">
                   <i className="fa-solid fa-quote-left fa-3x"></i>
                 </p>
               </div>
             </div>
-            <div>adsf</div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="keen-slider__slide number-slide">2</div>
-        <div className="keen-slider__slide number-slide">3</div>
-        <div className="keen-slider__slide number-slide">4</div>
-        <div className="keen-slider__slide number-slide">5</div>
-        <div className="keen-slider__slide number-slide">6</div>
+        <div className="keen-slider__slide number-slide">
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
+                <div className="divider my-0"></div>
+              </div>
+              <div className=" w-2/12">
+                <p className=" text-slate-400">
+                  <i className="fa-solid fa-quote-left fa-3x"></i>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
+                <div className="divider my-0"></div>
+              </div>
+              <div className=" w-2/12">
+                <p className=" text-slate-400">
+                  <i className="fa-solid fa-quote-left fa-3x"></i>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
+                <div className="divider my-0"></div>
+              </div>
+              <div className=" w-2/12">
+                <p className=" text-slate-400">
+                  <i className="fa-solid fa-quote-left fa-3x"></i>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
+                <div className="divider my-0"></div>
+              </div>
+              <div className=" w-2/12">
+                <p className=" text-slate-400">
+                  <i className="fa-solid fa-quote-left fa-3x"></i>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="keen-slider__slide number-slide">
+          <div className="w-full h-80 p-5 bg-white border-2 rounded-md">
+            <div className="flex justify-between">
+              <div className="w-9/12">
+                <h3 className="text-md text-black font-bold">Md Akash</h3>
+                <div className="divider my-0"></div>
+              </div>
+              <div className=" w-2/12">
+                <p className=" text-slate-400">
+                  <i className="fa-solid fa-quote-left fa-3x"></i>
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="">
+                He hit me with one the WORST pick up lines I had ever seen, but
+                being bored at work, I decided to reply. From there we never
+                stopped talking, dating each other and falling deeper in love.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <footer className="footer py-2">
+        <div className="container mx-auto w-full">
+          <div className="flex w-full border-b-2 pb-4">
+            <div className="w-3/12">
+              <h3 className="text-lg uppercase text-black font-bold mb-2">
+                About
+              </h3>
+              <ul className="flex flex-col justify-start items-start text-lg">
+                <li>Privacey</li>
+                <li>Terms</li>
+                <li>Cookie Policy</li>
+                <li>Intellectual Property</li>
+              </ul>
+            </div>
+            <div className="w-3/12">
+              <h3 className="text-lg uppercase text-black font-bold mb-2">
+                Careers
+              </h3>
+              <ul className="flex flex-col justify-start items-start text-lg">
+                <li>Careers Portal</li>
+                <li>Tech Blogs</li>
+              </ul>
+            </div>
+            <div className="w-3/12">
+              <h3 className="text-lg uppercase text-black font-bold mb-2">
+                Socials
+              </h3>
+              <ul className="flex  items-start ">
+                <li className="mx-2">
+                  <i className="fa-brands fa-instagram fa-2x"></i>
+                </li>
+                <li className="mx-2">
+                  <i className="fa-brands fa-tiktok fa-2x"></i>
+                </li>
+                <li className="mx-2">
+                  <i className="fa-brands fa-youtube fa-2x"></i>
+                </li>
+                <l className="mx-2">
+                  <i className="fa-brands fa-twitter fa-2x"></i>
+                </l>
+                <li className="mx-2">
+                  <i className="fa-brands fa-facebook-f fa-2x"></i>
+                </li>
+              </ul>
+            </div>
+            <div className="w-3/12">
+              <ul className="text-lg">
+                <li>FAQ</li>
+                <li className="mt-1">Destination</li>
+                <li className="mt-1">Press Room</li>
+                <li className="mt-1">Contact</li>
+                <li className="mt-1">Promocode</li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex items-center py-3">
+            <span className="text-lg font-bold uppercase block mr-4">
+              Get the app!
+            </span>
+            <Image
+              src="https://tinder.com/static/build/d256a5b510a685030be375c63a9010e9.webp"
+              alt="appstore"
+              height={50}
+              width={150}
+              className="block mx-2"
+            />
+            <Image
+              src="https://tinder.com/static/build/03aed264f8539c570e9bc80ba6b60d92.webp"
+              alt="appstore"
+              height={75}
+              width={160}
+              className="block mx-2"
+            />
+          </div>
+          <div className="border-b-2 pb-3">
+            <p>
+              Single people, listen up: If you’re looking for love, want to
+              start dating, or just keep it casual, you need to be on Tinder.
+              With over 55 billion matches made, it’s the place to be to meet
+              your next best match. Let’s be real, the dating landscape looks
+              very different today, as most people are meeting online. With
+              Tinder, the world’s most popular free dating app, you have
+              millions of other single people at your fingertips and they’re all
+              ready to meet someone like you. Whether you’re straight or in the
+              LGBTQIA community, Tinder’s here to bring you all the sparks.
+              <br />
+              <br />
+              There really is something for everyone on Tinder. Want to get into
+              a relationship? You got it. Trying to find some new friends? Say
+              no more. New kid on campus and looking to make the most of your
+              college experience? Tinder U’s got you covered. Tinder isn’t your
+              average dating site — it’s the most diverse dating app, where
+              adults of all backgrounds and experiences are invited to make
+              connections, memories, and everything in between.
+            </p>
+          </div>
+          <div className="w-full flex justify-between items-center py-4">
+            <div>
+              <ul className="flex ">
+                <li className="mx-1">FAQ </li>
+                <li className="mx-1">/ Safety Tips </li>
+                <li className="mx-1">/ Terms </li>
+                <li className="mx-1">/ Cookie Policy </li>
+                <li className="mx-1">/ Privecy Settings </li>
+              </ul>
+            </div>
+            <div>
+              <p>© 2022 Match Group, LLC, All Rights Reserved.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
